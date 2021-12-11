fef5824674dfdf919b8875c6a45c0146d6966139
chenzhongpu <chenloveit@gmail.com>
2021-12-11 23:02:58 +0800

    quick sort

--- src/sorting.rs
+++ src/sorting.rs
@@ -3,5 +3,6 @@ pub mod merge;
 pub mod merge_bu;
 pub mod merge_bu2;
 pub mod merge_slice;
+pub mod quick;
 pub mod selection;
 pub mod shell;
---
+++ src/sorting/quick.rs
@@ -0,0 +1,89 @@
+//! # Quick Sort
+
+use std::cmp::PartialOrd;
+
+pub fn sort<T: PartialOrd>(a: &mut [T]) {
+    // optional: random shuffle `a` to eliminate dependence on input
+    _sort(a, 0, a.len() - 1);
+}
+
+// note `j` can be `0`, so `lo` and `hi` should be `isize`.
+// another solution is to check whether `j` is `0`.
+fn _sort<T: PartialOrd>(a: &mut [T], lo: usize, hi: usize) {
+    if hi <= lo {
+        return;
+    }
+    let j = partition(a, lo, hi);
+    if j > 0 {
+        _sort(a, lo, j - 1);
+    }
+    _sort(a, j + 1, hi);
+}
+
+fn partition<T: PartialOrd>(a: &mut [T], lo: usize, hi: usize) -> usize {
+    let (mut i, mut j) = (lo, hi + 1);
+
+    loop {
+        i += 1;
+        while a[i] < a[lo] {
+            if i == hi {
+                break;
+            } else {
+                i += 1;
+            }
+        }
+        j -= 1;
+        while a[lo] < a[j] {
+            if j == lo {
+                break;
+            } else {
+                j -= 1;
+            }
+        }
+
+        if i >= j {
+            break;
+        }
+
+        a.swap(i, j);
+    }
+
+    a.swap(lo, j);
+    j
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sort_int() {
+        let mut v = vec![6, 2, 8, 1, 0, 9];
+        sort(&mut v);
+        assert_eq!(v, vec![0, 1, 2, 6, 8, 9]);
+    }
+
+    #[test]
+    fn sort_char() {
+        let mut v = vec!['S', 'O', 'R', 'T', 'E', 'X', 'A', 'M', 'P', 'L', 'E'];
+        sort(&mut v);
+        assert_eq!(
+            v,
+            vec!['A', 'E', 'E', 'L', 'M', 'O', 'P', 'R', 'S', 'T', 'X']
+        )
+    }
+
+    #[test]
+    fn ascending() {
+        let mut v = vec![0, 1, 3, 5, 7];
+        sort(&mut v);
+        assert_eq!(v, vec![0, 1, 3, 5, 7]);
+    }
+
+    #[test]
+    fn descending() {
+        let mut v = vec![7, 5, 3, 1, 0];
+        sort(&mut v);
+        assert_eq!(v, vec![0, 1, 3, 5, 7]);
+    }
+}
