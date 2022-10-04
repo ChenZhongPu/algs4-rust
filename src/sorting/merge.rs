24cd0a5dbe6956c7d50ccf851c270f2de5ca5982
CHEN zhongpu <chenloveit@gmail.com>
2022-10-04 21:20:40 +0800

    merge sort according to CLRS book

--- src/sorting/merge.rs
+++ src/sorting/merge.rs
@@ -1,60 +1,50 @@
 //! # Merge Sort
 //!
 //! Divide-and-conquer: sort the left half and right half, and then merge
-use std::cmp::PartialOrd;
+// use std::cmp::PartialOrd;
 
-pub fn sort<T: Copy + PartialOrd>(a: &mut [T]) {
-    let mut aux = a.to_vec();
-    _sort(a, &mut aux, 0, a.len() - 1);
+pub fn sort<T: Copy + PartialOrd + Default>(a: &mut [T]) {
+    merge_sort(a, 0, a.len() - 1);
 }
 
-fn merge<T: Copy + PartialOrd>(a: &mut [T], aux: &mut [T], lo: usize, mid: usize, hi: usize) {
-    let (mut i, mut j) = (lo, mid + 1);
+fn merge<T: Copy + PartialOrd + Default>(a: &mut [T], lo: usize, mid: usize, hi: usize) {
+    let (n_l, n_r) = (mid - lo + 1, hi - mid);
+    let mut left = vec![T::default(); n_l];
+    left.copy_from_slice(&a[lo..mid + 1]);
+    let mut right = vec![T::default(); n_r];
+    right.copy_from_slice(&a[mid + 1..hi + 1]);
 
-    aux[lo..(hi + 1)].copy_from_slice(&a[lo..(hi + 1)]);
-
-    // `cargo clippy` would complain:
-    // for k in lo..=hi {
-    //     // merge back to a[lo..hi]
-    //     if i > mid {
-    //         a[k] = aux[j];
-    //         j += 1;
-    //     } else if j > hi {
-    //         a[k] = aux[i];
-    //         i += 1;
-    //     } else if aux[j] < aux[i] {
-    //         a[k] = aux[j];
-    //         j += 1;
-    //     } else {
-    //         a[k] = aux[i];
-    //         i += 1;
-    //     }
-    // }
-    for x in a.iter_mut().take(hi + 1).skip(lo) {
-        if i > mid {
-            *x = aux[j];
-            j += 1;
-        } else if j > hi {
-            *x = aux[i];
+    let (mut i, mut j, mut k) = (0, 0, lo);
+    while i < n_l && j < n_r {
+        if left[i] <= right[j] {
+            a[k] = left[i];
             i += 1;
-        } else if aux[j] < aux[i] {
-            *x = aux[j];
-            j += 1;
         } else {
-            *x = aux[i];
-            i += 1;
+            a[k] = right[j];
+            j += 1;
         }
+        k += 1;
+    }
+
+    // copy the remainder
+    if i < n_l {
+        let remain = n_l - i;
+        a[k..remain + k].copy_from_slice(&left[i..]);
+    }
+    if j < n_r {
+        let remain = n_r - j;
+        a[k..remain + k].copy_from_slice(&right[j..]);
     }
 }
 
-fn _sort<T: Copy + PartialOrd>(a: &mut [T], aux: &mut [T], lo: usize, hi: usize) {
+fn merge_sort<T: Copy + PartialOrd + Default>(a: &mut [T], lo: usize, hi: usize) {
     if hi <= lo {
         return;
     }
     let mid = lo + (hi - lo) / 2;
-    _sort(a, aux, lo, mid);
-    _sort(a, aux, mid + 1, hi);
-    merge(a, aux, lo, mid, hi);
+    merge_sort(a, lo, mid);
+    merge_sort(a, mid + 1, hi);
+    merge(a, lo, mid, hi);
 }
 
 #[cfg(test)]
